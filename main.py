f93ef4af4dcd63b31e2fef4a5a06f81d27adb2be
Eshan-014 <92906270+Eshan-014@users.noreply.github.com>
2021-10-21 10:32:15 +0530

    Multiplication

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 a = input("Please enter a number")
 b = input ("Please enter a Number")
+c= input("Press 1 for Multiply Press 2 for addition")
+c = int(c)
 a = int(a)
 b = int(b)
-print(a+b)
-# Calculator for addition
+if c==1 :
+  print(a*b)
+if c == 2:
+  print (a+b)
+# Calculator for addition and multiplication
